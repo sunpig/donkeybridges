9362fe6e24d9d0dd596310e91e133c622aecb021
Martin Sutherland <martin@sunpig.com>
2011-06-15 01:58:07 +0200

    Fix url to project on github

--- partial/_end.php
+++ partial/_end.php
@@ -3,7 +3,7 @@
                     <div class="bd copy">
                         <p class="small"><a href="http://donkeybridges.com/">DonkeyBridges.com</a> by <a href="http://sunpig.com/martin/">Martin Sutherland</a> (<a class="twitter" href="http://twitter.com/intent/user?user_id=12627">@sunpig</a>) is a 
                             demonstration of the "link phrase" technique for uniquely identifying content on a web site.</p>
-                            <p class="small">Source code on <a href="http://github.com/sunpig/rmdemo">Github</a></p>
+                            <p class="small">Source code on <a href="http://github.com/sunpig/donkeybridges">Github</a></p>
                             <p class="small">Geographic data by <a href="http://developer.yahoo.com/geo/geoplanet/">Yahoo</a></p>
                             <p class="small">Geo code based on the <a href="http://isithackday.com/geoplanet-explorer/">GeoPlanet Explorer</a> by <a href="http://wait-till-i.com/">Chris Heilmann</a></p>
                         <p class="small">Background image of Earth from space: STS106-704-092 NASA/courtesy of <a href="http://nasaimages.org/">nasaimages.org</a></p>
