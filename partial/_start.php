382eebc76faa64d090023c5842b95e0d99224cbc
Martin Sutherland <martin@sunpig.com>
2011-06-15 13:15:04 +0200

    GA tracking

--- partial/_start.php
+++ partial/_start.php
@@ -5,7 +5,20 @@
         <title><?php echo $pageTitle ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/css-all.php" />
-    </head>
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-10334221-3']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
+        </head>
     <body class="page">
         <div class="pageContainer">
             <header class="mod mod-rm mod-rm-dark">
