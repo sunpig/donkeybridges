8ebc285aa0ec96550fba68acbc1efaaf356a34f2
Martin Sutherland <martin@sunpig.com>
2016-05-29 11:54:15 +0200

    Fix links

--- app/partial/_end.php
+++ app/partial/_end.php
@@ -1,9 +1,9 @@
             <footer class="mod mod-rm mod-rm-dark">
                 <div class="inner mam">
                     <div class="bd copy">
-                        <p class="small"><a href="http://donkeybridges.com/">DonkeyBridges.com</a> by <a href="http://sunpig.com/martin/">Martin Sutherland</a> (<a class="twitter" href="http://twitter.com/intent/user?user_id=12627">@sunpig</a>) is a 
-                            demonstration of the <a href="http://www.sunpig.com/martin/archives/2011/06/15/donkey-bridges.html">"link phrase" technique for uniquely identifying content on a web site</a>.</p>
-                            <p class="small">Source code on <a href="http://github.com/sunpig/donkeybridges">Github</a></p>
+                        <p class="small"><a href="http://donkeybridges.com/">DonkeyBridges.com</a> by <a href="https://sunpig.com/martin/">Martin Sutherland</a> (<a class="twitter" href="http://twitter.com/intent/user?user_id=12627">@sunpig</a>) is a 
+                            demonstration of the <a href="https://sunpig.com/martin/2011/06/15/donkey-bridges/">"link phrase" technique for uniquely identifying content on a web site</a>.</p>
+                            <p class="small">Source code on <a href="https://github.com/sunpig/donkeybridges">Github</a></p>
                             <p class="small">Geographic data by <a href="http://developer.yahoo.com/geo/geoplanet/">Yahoo</a></p>
                             <p class="small">Geo code based on the <a href="http://isithackday.com/geoplanet-explorer/">GeoPlanet Explorer</a> by <a href="http://wait-till-i.com/">Chris Heilmann</a></p>
                         <p class="small">Background image of Earth from space: STS106-704-092 NASA/courtesy of <a href="http://nasaimages.org/">nasaimages.org</a></p>
